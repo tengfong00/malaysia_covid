546c292c5f8faefbb42ccae7ccc3036a8f894904
SimpsonFong <sf_ngoitengfong@qiup.edu.my>
2021-01-25 15:50:26 +0800

    Remove some comments and spacing

--- main.py
+++ main.py
@@ -39,65 +39,62 @@ def get_iframe(target):
     
 
 if __name__ == '__main__':
-        target = 'http://covid-19.moh.gov.my/'
+    target = 'http://covid-19.moh.gov.my/'
 
-        req = requests.get(url = target)
-        req.encoding = 'utf-8'
-        html = req.text
-        bs = BeautifulSoup(html, 'lxml')
+    req = requests.get(url = target)
+    req.encoding = 'utf-8'
+    html = req.text
+    bs = BeautifulSoup(html, 'lxml')
 
-        #Get Infogram
-        texts = bs.find('section', id='g-intro')
-        infogram_script = texts.find('script')
-        infogram_id = infogram_script['id']
-        infogram_id = re.search("(?<=_)[^_]+$", infogram_id).group()
+    #Get Infogram
+    texts = bs.find('section', id='g-intro')
+    infogram_script = texts.find('script')
+    infogram_id = infogram_script['id']
+    infogram_id = re.search("(?<=_)[^_]+$", infogram_id).group()
+    
+    #IframeURL
+    iframeUrl = 'https://e.infogram.com/' + infogram_id
+
+    content = get_iframe(iframeUrl)
+    path = '/stat.json'
+    # JSON 
+    if os.path.getsize('stat.json') > 0:
+        json_output = {
+            "TotalKes" : content[0],
+            "ActiveKes" : content[1],
+            "Dead" : content[2],
+            "Recovered" : content[3],
+            "Date": content[4]
+        }
+
+        with open('stat.json') as json_file:
+            data = json.load(json_file)
+            temp = data['MalaysiaStat']
+            temp.append(json_output)
+
+        with open('stat.json', 'w') as f:
+            json.dump(data, f, indent = 4)
+        print ("Output1")
+    else:
+        json_output = {
+            "MalaysiaStat": [
+                {
+                    "TotalKes" : content[0],
+                    "ActiveKes" : content[1],
+                    "Dead" : content[2],
+                    "Recovered" : content[3],
+                    "Date": content[4]
+                }
+            ]
+        }
+
+        json_write = json.dumps(json_output, indent = 4)
         
-        #IframeURL
-        iframeUrl = 'https://e.infogram.com/' + infogram_id
+        with open('stat.json', 'w') as outfile:
+            outfile.write(json_write)
+        print("Output2")
 
-        content = get_iframe(iframeUrl)
-        path = '/stat.json'
-        # JSON 
-        if os.path.getsize('stat.json') > 0:
-            json_output = {
-                "TotalKes" : content[0],
-                "ActiveKes" : content[1],
-                "Dead" : content[2],
-                "Recovered" : content[3],
-                "Date": content[4]
-            }
-
-            with open('stat.json') as json_file:
-                data = json.load(json_file)
-                temp = data['MalaysiaStat']
-                temp.append(json_output)
-
-            with open('stat.json', 'w') as f:
-                json.dump(data, f, indent = 4)
-            print ("Output1")
-        else:
-            json_output = {
-                "MalaysiaStat": [
-                    {
-                        "TotalKes" : content[0],
-                        "ActiveKes" : content[1],
-                        "Dead" : content[2],
-                        "Recovered" : content[3],
-                        "Date": content[4]
-                    }
-                ]
-            }
-
-            json_write = json.dumps(json_output, indent = 4)
-            
-            with open('stat.json', 'w') as outfile:
-                outfile.write(json_write)
-            print("Output2")
-
-
-
-        # with open('stat.json') as json_file:
-        #     bata = json.load(json_file)
-            
-        #     print (bata['MalaysiaStat'][0]['Dead'])
-        #     print (type(bata['MalaysiaStat']))
+    # with open('stat.json') as json_file:
+    #     bata = json.load(json_file)
+        
+    #     print (bata['MalaysiaStat'][0]['Dead'])
